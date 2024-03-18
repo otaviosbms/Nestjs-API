51027648084ccb9ade069c7f87898edff199cd21
otaviopedbot <otavio.santos@pedbot.com.br>
2024-03-18 17:11:34 -0300

    first-commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Post, Get, Body, Param, Put, Patch, Delete, ParseIntPipe } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { UserService } from "./user.service";
+
+
+@Controller('users')
+export class UserController {
+
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    async create(@Body() data: CreateUserDTO) {
+        return this.userService.create(data);
+    }
+
+    @Get()
+    async list() {
+        return this.userService.list();
+    }
+
+    @Get(':id')
+    async show(@Param('id', ParseIntPipe) id: number) {
+        return this.userService.show(id);
+    }
+
+    @Put(':id')
+    async update(@Body() data: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
+        return this.userService.update(id, data);
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() data: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
+        return this.userService.updatePartial(id, data);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id: number) {
+        return this.userService.delete(id);
+    }
+
+
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,87 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+
+@Injectable()
+export class UserService {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create(data: CreateUserDTO) {
+
+        return this.prisma.user.create({
+            data: data
+        });
+
+    }
+
+    async list() {
+
+        return this.prisma.user.findMany();
+    }
+
+    async show(id: number) {
+        return this.prisma.user.findUnique({
+            where: { id, }
+        })
+    }
+
+    async update(id: number, { name, email, password, birthAt }: UpdatePutUserDTO) {
+
+        return this.prisma.user.update({
+            data: { name, email, password, birthAt: birthAt ? new Date(birthAt) : null },
+            where: {
+                id
+            }
+        })
+    }
+
+    async updatePartial(id: number, { name, email, password, birthAt }: UpdatePatchUserDTO) {
+
+        const data: any = {};
+
+        if (birthAt) {
+            data.birthAt = new Date(birthAt)
+        }
+
+        if (email) {
+            data.email = email
+        }
+
+        if (name) {
+            data.name = name
+        }
+
+        if (password) {
+            data.password = password
+        }
+
+        return this.prisma.user.update({
+            data,
+            where: {
+                id
+            }
+        })
+    }
+
+    async delete(id: number) {
+
+        await this.exists(id);
+
+        return this.prisma.user.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+    async exists(id: number) {
+        if (!(await this.show(id))) {
+            throw new NotFoundException(`O usuário ${id} não existe`);
+        }
+    }
+
+
+}
